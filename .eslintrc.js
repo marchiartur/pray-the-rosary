d0ad1cf2f38dc205b4b799ddcde7327223335f05
Artur Pacagnan <marchiartur@gmail.com>
2023-03-30 23:36:12 -0300

    adding husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,33 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: [
-    'plugin:react/recommended',
-    'standard-with-typescript',
-    'plugin:@next/next/recommended',
-    'eslint:recommended'
-  ],
-  overrides: [
-  ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 'latest',
+    ecmaVersion: 2021,
     sourceType: 'module',
     warnOnUnsupportedTypeScriptVersion: false,
-    project: ['tsconfig.json'],
+    project: ['./tsconfig.json'],
+    emitDecoratorMetadata: true,
     ecmaFeatures: {
       jsx: true
     }
   },
-  plugins: [
-    'react'
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    'plugin:@next/next/recommended'
   ],
+  plugins: ['react'],
   rules: {
+    '@typescript-eslint/key-spacing': 'error',
+    "no-use-before-define": ["error", { "variables": false }],
+    "@typescript-eslint/key-spacing": "off",
+    "no-use-before-define": "off",
+    "@typescript-eslint/no-use-before-define": ["error"]
+  },
+  settings: {
+    "react": {
+      "version": "detect"
+    }
   },
   globals: {
     JSX: true
